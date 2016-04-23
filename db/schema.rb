b0cd53de966e3b377684f4000969b647de799c4b
Poilon <poilon@gmail.com>
2016-04-23 20:10:34 +0200

    ws

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160405060724) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.string "body"
     t.string "username"
